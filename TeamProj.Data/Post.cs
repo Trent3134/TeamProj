8bf5e86223f82c3dbfb97d70d9676d9f08a1af1b
daking550 <david.king550wil@gmail.com>
2022-06-09 18:48:48 -0400

    David and post class

---
+++ TeamProj.Data/Post.cs
@@ -0,0 +1,7 @@
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+    //public virtual List<Comments> Comments {get; set;}
+}
