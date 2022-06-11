e57c28e9b6c35cf865fd7c2b99ff6d4ee340ad20
daking550 <david.king550wil@gmail.com>
2022-06-11 10:46:33 -0400

    Authorize

--- TeamProjWebApi/Controllers/PostController.cs
+++ TeamProjWebApi/Controllers/PostController.cs
@@ -1,6 +1,11 @@
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class PostController : ControllerBase
 {
     private readonly IPostService _postService;
+    public PostController (IPostService postService)
+    {
+        _postService = postService;
+    }
 }
